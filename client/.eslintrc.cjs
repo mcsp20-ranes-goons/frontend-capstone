33ff2d8d35c6614042831c57aaa78f22ad6363d7
Rane Gray <ranegray830@gmail.com>
2023-05-11 17:28:05 +0200

    deleted everything and started anew with tailwind

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,27 +1,15 @@
-/* eslint-disable no-undef */
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: {
-    react: { version: "18.2" },
-    "import/resolver": {
-      "node": {
-        "paths": ["src"],
-        "extensions": [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
-  plugins: ["react-refresh"],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': 'warn',
   },
-};
+}
