4eb3cfc902aaf5053a2d5d86f80de485c732976a
Rane Gray <ranegray830@gmail.com>
2023-05-11 21:07:58 +0200

    added rtl and vitest w test tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
-  return <></>;
+function App({ title }) {
+  return (
+    <div className="text-2xl">
+      <h1>Hello world!</h1>
+      <h2>{title}</h2>
+    </div>
+  );
 }
 
 export default App;
