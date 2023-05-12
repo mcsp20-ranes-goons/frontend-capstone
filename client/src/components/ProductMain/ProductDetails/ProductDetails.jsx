d7eff2a1fbfb9c3a25b8a0efb68b78fb74f6bcdc
Rane Gray <ranegray830@gmail.com>
2023-05-12 20:07:37 +0200

    wip scaffolding product details component

--- client/src/components/ProductMain/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductMain/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,14 @@
 function ProductDetails() {
     return (
-      <div>
-          <h1>ProductDetails</h1>
+      <div className="bg-neutral-500">
+          <div>
+            <img src="https://cdn2.unrealengine.com/egs-starwarsjedisurvivorstandardedition-respawnentertainment-ic1-400x400-9ff568e5738d.png?h=270&quality=medium&resize=1&w=480" alt="" />
+          </div>
+          <div>
+            <img src="https://cdn1.epicgames.com/gameRating/gameRating/ESRB_T_192_192x192-a0885066a34083cd647dc10db21717f1" alt="" />
+            <p>Teen</p>
+            <p>Violence, Mild Language</p>
+          </div>
       </div>
     );
   }
