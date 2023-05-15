8278c158330b6e82af7832cdc79ee90f0e5a229d
Rusty12321 <weston@hilgenfeld.org>
2023-05-15 18:10:30 -0500

    removed comments and changed a span to p

--- client/src/components/ProductMain/ProductColumn/Specifications/Specifications.jsx
+++ client/src/components/ProductMain/ProductColumn/Specifications/Specifications.jsx
@@ -3,92 +3,75 @@ function Specifications() {
     <div className="flex flex-col gap-4">
       <h1 className="text-xl">Specifications</h1>
       <div className="flex flex-col bg-neutral-800 py-9 px-16">
-        {/*container*/}
-        <div className="flex">{/*header-container*/}
-          <button className="flex border-b-4 transition-all ease-linear hover:border-neutral-400">{/*windows*/}
+        <div className="flex">
+          <button className="flex border-b-4 transition-all ease-linear hover:border-neutral-400">
             <span className="font-bold my-5">WINDOWS</span>
           </button>
           <div className="mb-[0.1rem] w-full border-b border-neutral-700"></div>
         </div>
         <div className="flex flex-row justify-between mt-10">
-          {/*specs*/}
           <div className="flex flex-col gap-10">
-            {/*minimum*/}
             <span className="text-neutral-400">MINIMUM</span>
             <div className="flex flex-col">
-              {/*OS*/}
               <span className="text-neutral-400">OS</span>
               <span>Windows 10 64-bit</span>
             </div>
             <div className="flex flex-col">
-              {/*Processor*/}
               <span className="text-neutral-400">Processor</span>
               <span>
                 4 core / 8 threads | Intel Core i7-7700 | Ryzen 5 1400
               </span>
             </div>
             <div className="flex flex-col">
-              {/*memory*/}
               <span className="text-neutral-400">Memory</span>
               <span>8 GB</span>
             </div>
             <div className="flex flex-col">
-              {/*storage*/}
               <span className="text-neutral-400">Storage</span>
               <span>155 GB</span>
             </div>
             <div className="flex flex-col">
-              {/*direct x*/}
               <span className="text-neutral-400">Direct X</span>
               <span>12</span>
             </div>
           </div>
           <div className="flex flex-col gap-10">
-            {/*recommended*/}
             <span className="text-neutral-400">RECOMMENDED</span>
             <div className="flex flex-col">
-              {/*OS*/}
               <span className="text-neutral-400">OS</span>
               <span>Windows 10 64-bit</span>
             </div>
             <div className="flex flex-col">
-              {/*Processor*/}
               <span className="text-neutral-400">Processor</span>
               <span>
                 4 core / 8 threads | Intel Core i5 11600K | Ryzen 5 5600X
               </span>
             </div>
             <div className="flex flex-col">
-              {/*memory*/}
               <span className="text-neutral-400">Memory</span>
               <span>16 GB</span>
             </div>
             <div className="flex flex-col">
-              {/*storage*/}
               <span className="text-neutral-400">Storage</span>
               <span>155 GB</span>
             </div>
             <div className="flex flex-col">
-              {/*direct x*/}
               <span className="text-neutral-400">Direct X</span>
               <span>12</span>
             </div>
           </div>
         </div>
         <div className="flex flex-col mt-10">
-          {/*logins*/}
           <span className="text-neutral-400">logins</span>
           <span>Requires EA Origin account</span>
         </div>
         <div className="flex flex-col mt-10 pb-10 border-b border-neutral-700">
-          {/*languages*/}
           <span className="text-neutral-400">Languages Supported</span>
           <span>AUDIO: English, Italian, French, German, Spanish - Spain, Spanish - Latin America, Polish, Japanese, Portuguese - Brazil</span>
           <span className="max-w-4xl">TEXT: English, French, Italian, German, Spanish - Latin America, Spanish - Spain, Portuguese - Brazil, Polish, Japanese, Korean, Chinese - Simplified, Chinese - Traditional</span>
         </div>
-        <div className="flex flex-col items-center mt-10">
-          {/*privacyPolicy*/}
-          <span className="text-center text-neutral-400 max-w-4xl">
+        <div className="flex flex-col items-center mt-10 gap-1">
+          <p className="text-center text-neutral-400 max-w-4xl">
             Internet connection, EA Account, Epic Games Account, acceptance of
             the EA User Agreement (terms.ea.com), and installation of the EA app
             (ea.com/ea-app) are required to play. EA’s Privacy and Cookie Policy
@@ -115,7 +98,7 @@ function Specifications() {
             copyrights, in the United States and other countries, of Lucasfilm
             Ltd. and/or its affiliates. © & TM 2023 Lucasfilm Ltd. All rights
             reserved.
-          </span>
+          </p>
           <a href="https://www.ea.com/legal" target="_blank">Privacy Policy</a>
         </div>
       </div>
