fb0a0141151830fb1e224014644b406a7112b73c
Rusty12321 <weston@hilgenfeld.org>
2023-05-15 18:36:27 -0500

    added follow us section

--- client/src/components/ProductMain/ProductColumn/FollowUs/FollowUs.jsx
+++ client/src/components/ProductMain/ProductColumn/FollowUs/FollowUs.jsx
@@ -1,9 +1,15 @@
 function FollowUs() {
-    return (
-      <div>
-          <h1>FollowUs</h1>
+  return (
+    <div>
+      <h1 className="text-xl mb-5">FollowUs</h1>
+      <div className="flex justify-center gap-4 py-8 mb-5 bg-neutral-800">
+        <div>facebook</div>
+        <div>playstation</div>
+        <div>twitter</div>
+        <div>youtube</div>
       </div>
-    );
-  }
-  
-  export default FollowUs;
+    </div>
+  );
+}
+
+export default FollowUs;
