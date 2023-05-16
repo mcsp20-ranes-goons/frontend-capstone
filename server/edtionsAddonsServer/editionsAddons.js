4f4c791fe22bf5e3bdff8f37ec64973f74cdffdf
Rusty12321 <weston@hilgenfeld.org>
2023-05-16 15:51:38 -0500

    added server for EditionsAddons component

---
+++ server/edtionsAddonsServer/editionsAddons.js
@@ -0,0 +1,53 @@
+import pg from "pg";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const { Pool } = pg;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/editions/:productID", (req, res) => {
+  pool
+    .query('SELECT * from "editions" WHERE "product_ID" = $1', [
+      req.params.productID,
+    ])
+    .then((result) => {
+      if (result.rows.length == 0) {
+        res.status(404).send("No Editions Found For This Product");
+      } else {
+        res.send(result.rows);
+      }
+    });
+});
+
+app.get("/api/add-ons/:productID", (req, res) => {
+  pool
+    .query('SELECT * from "addOns" WHERE "product_ID" = $1', [
+      req.params.productID,
+    ])
+    .then((result) => {
+      if (result.rows.length == 0) {
+        res.status(404).send("No Editions Found For This Product");
+      } else {
+        res.send(result.rows);
+      }
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
