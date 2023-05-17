df1b87b96c973bbb797098f0d78f9759e63864ba
Rusty12321 <weston@hilgenfeld.org>
2023-05-17 15:01:55 -0500

    removed hard coded links and adjusted for conditional rendering

--- client/src/components/ProductMain/ProductColumn/EditionsAddons/EditionsAddons.jsx
+++ client/src/components/ProductMain/ProductColumn/EditionsAddons/EditionsAddons.jsx
@@ -6,68 +6,88 @@ function EditionsAddons() {
 
   useEffect(() => {
     fetch("http://localhost:3001/api/editions/1")
-      .then((result) => result.json())
-      .then((data) => setEditions(data));
+      .then((result) => {
+        if (result.status === 404) {
+          return [];
+        } else {
+          return result.json()
+        }
+      })
+      .then((data) => setEditions(data))
+      
 
     fetch("http://localhost:3001/api/add-ons/1")
-      .then((result) => result.json())
+      .then((result) => {
+        if (result.status === 404) {
+          return [];
+        } else {
+          return result.json()
+        }
+      })
       .then((data) => setAddOns(data));
   }, []);
-  
-  let display = (item) => { 
+
+  const display = (item) => {
     let type = "EDITION";
     if (!item.name.toLowerCase().includes("edition")) {
-      type = "ADD-ON"
+      type = "ADD-ON";
     }
     return (
-    <div className="flex flex-col mb-5">
-      <div className="flex bg-neutral-800 border-b border-neutral-700">
-        <img
-          className="w-72 h-40"
-          src={item.image}
-          alt={item.name}
-        />
-        <div>
-          <div className="flex flex-row">
-            <div className="flex flex-row justify-center items-center py-2 m-3 px-4 bg-neutral-700 rounded-sm text-sm">
-              {item.name.toLowerCase().includes("standard") ? "BASE GAME" : type}
+      <div className="flex flex-col mb-5" key={item.id}>
+        <div className="flex bg-neutral-800 border-b border-neutral-700">
+          <a href={item.pageUrl} target="_blank">
+            <img className="w-72 h-40" src={item.image} alt={item.name} />
+          </a>
+          <div>
+            <div className="flex flex-row">
+              <div className="flex flex-row justify-center items-center py-2 m-3 px-4 bg-neutral-700 rounded-sm text-sm">
+                {item.name.toLowerCase().includes("standard")
+                  ? "BASE GAME"
+                  : type}
+              </div>
+              <div className="flex items-center m-2">
+                <a href={item.pageUrl} target="_blank">
+                  {item.name}
+                </a>
+              </div>
             </div>
-            <div className="flex items-center m-2">
-              <a href="https://store.epicgames.com/en-US/p/star-wars-jedi-survivor">
-                {item.name}
-              </a>
+            <div className="flex items-center px-3">
+              <p className="max-w-lg text-neutral-400">{item.description}</p>
             </div>
-          </div>
-          <div className="flex items-center px-3">
-            <p className="text-neutral-400">
-              {item.description}
-            </p>
           </div>
         </div>
+        <div className="flex flex-row justify-end items-center p-5 gap-3 bg-neutral-800">
+          <div className="text-lg">${item.price}</div>
+          <button className="flex justify-center items-center px-12 py-3 border border-neutral-700 rounded-sm transition-all ease-linear hover:bg-neutral-700">
+            ADD TO CART
+          </button>
+          <button className="flex justify-center items-center px-8 py-3 border border-neutral-700 rounded-sm transition-all ease-linear hover:bg-neutral-700">
+            ADD TO WISHLIST
+          </button>
+        </div>
       </div>
-      <div className="flex flex-row justify-end items-center p-5 gap-3 bg-neutral-800">
-        <div className="text-lg">${item.price}</div>
-        <button className="flex justify-center items-center px-12 py-3 border border-neutral-700 rounded-sm transition-all ease-linear hover:bg-neutral-700">
-          ADD TO CART
-        </button>
-        <button className="flex justify-center items-center px-8 py-3 border border-neutral-700 rounded-sm transition-all ease-linear hover:bg-neutral-700">
-          ADD TO WISHLIST
-        </button>
-      </div>
-    </div>
-  )};
+    );
+  };
 
-
-  return (
-    <div>
+  const editionsAddons = {
+    editions: (
       <div>
         <h1 className="text-xl my-5">Editions</h1>
         {editions.map((edition) => display(edition))}
       </div>
+    ),
+    addons: (
       <div>
         <h1 className="text-xl my-5">Add-Ons</h1>
         {addOns.map((addon) => display(addon))}
       </div>
+    ),
+  };
+
+  return (
+    <div>
+      {editions.length !== 0 ? editionsAddons.editions : null}
+      {addOns.length !== 0 ? editionsAddons.addons : null}
     </div>
   );
 }
