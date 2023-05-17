250adf3a59a81e71b29f85335d68e53c39086d92
Rane Gray <ranegray830@gmail.com>
2023-05-17 23:20:24 +0200

    added client side logic for ratings component

--- client/src/components/ProductMain/ProductColumn/Ratings/Ratings.jsx
+++ client/src/components/ProductMain/ProductColumn/Ratings/Ratings.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 
 function Ratings() {
+  const [ratings, setRatings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/ratings/1")
+      .then((res) => res.json())
+      .then((data) => setRatings(data))
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <div>
       <h1 className="text-xl">Ratings</h1>
@@ -12,7 +22,7 @@ function Ratings() {
                 89%
               </div>
             </div>
-            <p>Critics Reccomend</p>
+            <p>Critics Recommend</p>
           </div>
           <div className="flex flex-col items-center gap-1">
             <div className="bg-blue-500 h-[4.5rem] w-[4.5rem] rounded-full flex justify-center items-center">
@@ -46,84 +56,27 @@ function Ratings() {
       </div>
 
       <div className="flex gap-4 my-4">
-        <div className="bg-neutral-800 p-4">
-          <h2>Game Informer</h2>
-          <h3 className="text-neutral-400">by Matt Miller</h3>
+        {ratings.map(rating => (
+        <div key={rating.id} className="bg-neutral-800 w-1/3 p-4">
+          <h2>{rating.title}</h2>
+          <h3 className="text-neutral-400">by {rating.author}</h3>
           <hr className="border-neutral-700 my-4" />
-          <p className="mb-4">9.25/10</p>
+          <p className="mb-4">{rating.rating}</p>
           <p className="text-neutral-400 mb-4">
-            Developer Respawn Entertainment clearly took a measured and
-            thoughtful approach to analyze what worked and what didn't in its
-            last Star Wars game, and Jedi: Survivor feels like a worthy attempt
-            at evolution. It captures the magic of Star Wars as well as anything
-            in the current canon, and it's a stellar adventure in its own right.
+            {rating.review}
           </p>
           <p>
             <a
               className="underline flex items-center text-neutral-400 gap-1"
-              href={
-                {
-                  /* TODO */
-                }
-              }
+              href={rating.url}
             >
               READ FULL REVIEW
               <ArrowTopRightOnSquareIcon className="h-4 w-4" />
             </a>
           </p>
         </div>
-        <div className="bg-neutral-800 p-4">
-          <h2>Game Informer</h2>
-          <h3 className="text-neutral-400">by Matt Miller</h3>
-          <hr className="border-neutral-700 my-4" />
-          <p className="mb-4">9.25/10</p>
-          <p className="text-neutral-400 mb-4">
-            Developer Respawn Entertainment clearly took a measured and
-            thoughtful approach to analyze what worked and what didn't in its
-            last Star Wars game, and Jedi: Survivor feels like a worthy attempt
-            at evolution. It captures the magic of Star Wars as well as anything
-            in the current canon, and it's a stellar adventure in its own right.
-          </p>
-          <p>
-            <a
-              className="underline flex items-center text-neutral-400 gap-1"
-              href={
-                {
-                  /* TODO */
-                }
-              }
-            >
-              READ FULL REVIEW
-              <ArrowTopRightOnSquareIcon className="h-4 w-4" />
-            </a>
-          </p>
-        </div>
-        <div className="bg-neutral-800 p-4">
-          <h2>Game Informer</h2>
-          <h3 className="text-neutral-400">by Matt Miller</h3>
-          <hr className="border-neutral-700 my-4" />
-          <p className="mb-4">9.25/10</p>
-          <p className="text-neutral-400 mb-4">
-            Developer Respawn Entertainment clearly took a measured and
-            thoughtful approach to analyze what worked and what didn't in its
-            last Star Wars game, and Jedi: Survivor feels like a worthy attempt
-            at evolution. It captures the magic of Star Wars as well as anything
-            in the current canon, and it's a stellar adventure in its own right.
-          </p>
-          <p>
-            <a
-              className="underline flex items-center text-neutral-400 gap-1"
-              href={
-                {
-                  /* TODO */
-                }
-              }
-            >
-              READ FULL REVIEW
-              <ArrowTopRightOnSquareIcon className="h-4 w-4" />
-            </a>
-          </p>
-        </div>
+
+        ))}
       </div>
       <p className="text-sm text-neutral-400">Reviews provided by OpenCritic</p>
     </div>
