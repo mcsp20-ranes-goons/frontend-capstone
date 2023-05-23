73f033c83c52959afd48e5edb7c2bfdf98c45aa4
Rusty12321 <weston@hilgenfeld.org>
2023-05-22 19:33:22 -0500

    adjusted border to be rounded correctly

--- client/src/components/ProductMain/ProductColumn/EditionsAddons/EditionsAddons.jsx
+++ client/src/components/ProductMain/ProductColumn/EditionsAddons/EditionsAddons.jsx
@@ -33,9 +33,9 @@ function EditionsAddons({ id }) {
     }
     return (
       <div className="flex flex-col mb-5" key={item.id}>
-        <div className="flex bg-neutral-800 border-b border-neutral-700 rounded">
+        <div className="flex bg-neutral-800 border-b border-neutral-700 rounded-t">
           <a href={item.pageUrl} target="_blank">
-            <img className="w-72 h-40" src={item.image} alt={item.name} />
+            <img className="w-72 h-40 rounded-ss" src={item.image} alt={item.name} />
           </a>
           <div>
             <div className="flex flex-row">
@@ -55,7 +55,7 @@ function EditionsAddons({ id }) {
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-end items-center p-5 gap-3 bg-neutral-800">
+        <div className="flex flex-row justify-end items-center p-5 gap-3 bg-neutral-800 rounded-b">
           <div className="text-lg">${item.price}</div>
           <button className="flex justify-center items-center px-12 py-3 border border-neutral-700 rounded-sm transition-all ease-linear hover:bg-neutral-700">
             ADD TO CART
